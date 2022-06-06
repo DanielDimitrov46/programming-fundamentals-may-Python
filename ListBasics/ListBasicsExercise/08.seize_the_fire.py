f05995fda01caa5b3dc762885702df11fae64277
DanielDimitrov46 <dani.dimitrov.snack9@gmail.com>
2022-06-06 23:30:07 +0300

    Update 08.seize_the_fire.py

---
+++ ListBasics/ListBasicsExercise/08.seize_the_fire.py
@@ -0,0 +1,22 @@
+sort_of_fire = input().split("#")
+amount_of_water = int(input())
+put_out_cells = []
+
+for element in sort_of_fire:
+    current_cell = element.split()
+    type_of_fire = current_cell[0]
+    value_of_fire = int(current_cell[4])
+    if (type_of_fire == "High") and (81 <= value_of_fire <= 125):
+        amount_of_water-=value_of_fire
+    else:
+        pass
+
+    if (type_of_fire == "Medium") and (51 <= value_of_fire <= 80):
+        amount_of_water-=value_of_fire
+    else:
+        pass
+
+    if (type_of_fire == "Low") and (1 <= value_of_fire <= 50):
+        amount_of_water-=value_of_fire
+    else:
+        pass
