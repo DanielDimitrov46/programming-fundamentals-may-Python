7495097d4ff29e48ea9fd756536d584908e1256c
DanielDimitrov46 <dani.dimitrov.snack9@gmail.com>
2022-06-10 23:51:16 +0300

    work in functions

--- ListBasics/ListBasicsExercise/08.seize_the_fire.py
+++ ListBasics/ListBasicsExercise/08.seize_the_fire.py
@@ -1,13 +1,17 @@
 sort_of_fire = input().split("#")
 amount_of_water = int(input())
 put_out_cells = []
+effort = 0
 
 for element in sort_of_fire:
     current_cell = element.split()
     type_of_fire = current_cell[0]
     value_of_fire = int(current_cell[4])
+    if amount_of_water<=0:
+        break
     if (type_of_fire == "High") and (81 <= value_of_fire <= 125):
         amount_of_water-=value_of_fire
+        #effort+=0.25*value_of_fire
     else:
         pass
 
