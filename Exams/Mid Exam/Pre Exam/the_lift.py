73efab5d8771a1e2bbad2e8ed07ae7a663e57706
DanielDimitrov46 <dani.dimitrov.snack9@gmail.com>
2022-06-24 23:42:58 +0300

    ref

--- Exams/Mid Exam/Pre Exam/the_lift.py
+++ Exams/Mid Exam/Pre Exam/the_lift.py
@@ -4,24 +4,25 @@ counter =0
 flag = True
 
 for current_wagon in wagons_list:
-    if number_of_people > 0:
-        if current_wagon == 0:
-            if number_of_people<4:
-                current_wagon += number_of_people
-                number_of_people-=number_of_people
-            else:
-                current_wagon+=4
-                number_of_people -= 4
-            wagons_list[counter] = current_wagon
-        elif current_wagon != 0:
-            diff = 4 - current_wagon
-            current_wagon += diff
-            number_of_people -= diff
-            wagons_list[counter] = current_wagon
-    counter+=1
+    if current_wagon== 0:
+        if number_of_people>=4: 
+            number_of_people-=4
+            current_wagon+=4
+        else:
+            current_wagon+=number_of_people
+            number_of_people = 0
+        wagons_list[counter] += current_wagon
+    else:
+        difference = 4-current_wagon
+        current_wagon+=difference
+        number_of_people-=difference
+        wagons_list[counter] = current_wagon
+    counter+=1 
 if number_of_people>0:
     print(f"There isn't enough space! {number_of_people} people in a queue!")
     print(' '.join([str(num) for num in wagons_list]))
 elif sum(wagons_list)!=len(wagons_list)*4:
     print("The lift has empty spots!")
+    print(' '.join([str(num) for num in wagons_list]))
+else:
     print(' '.join([str(num) for num in wagons_list]))
